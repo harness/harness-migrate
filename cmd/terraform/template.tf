2af83d4a20f6fe204cb539576961dc6e4a16deb9
Jim Sheldon <jim.sheldon@gmail.com>
2023-05-05 17:10:30 -0400

    working pr/push/tag triggers

--- cmd/terraform/template.tf
+++ cmd/terraform/template.tf
@@ -101,6 +101,132 @@ ${each.value.yaml_stages}
 EOT
 }
 
+module "trigger_pr" {
+  for_each = local.projects
+  
+  source  = "harness-community/content/harness//modules/triggers"
+  version = "~> 0.1"
+
+  name = "Pull Request"
+  organization_id = module.organization.details.id
+  project_id      = module.projects[each.key].details.id
+  pipeline_id     = module.pipelines[each.key].details.id
+  yaml_data       = <<-EOT
+source:
+  type: Webhook
+  spec:
+    # TODO: support other SCM types
+    type: Github
+    spec:
+      type: PullRequest
+      spec:
+        connectorRef: {{ $.Connectors.Repo }}
+        autoAbortPreviousExecutions: false
+        payloadConditions:
+          - key: targetBranch
+            operator: Equals
+            value: ${each.value.branch}
+        headerConditions: []
+        repoName: {{ $.Org.Name }}/${each.key}
+        actions:
+          - Open
+          - Reopen
+          - Synchronize
+inputYaml: |
+  pipeline:
+    identifier: ${module.pipelines[each.key].details.id}
+    properties:
+      ci:
+        codebase:
+          build:
+            type: PR
+            spec:
+              number: <+trigger.prNumber>
+  EOT
+}
+
+module "trigger_push" {
+  for_each = local.projects
+  
+  source  = "harness-community/content/harness//modules/triggers"
+  version = "~> 0.1"
+
+  name = "Push"
+  organization_id = module.organization.details.id
+  project_id      = module.projects[each.key].details.id
+  pipeline_id     = module.pipelines[each.key].details.id
+  yaml_data       = <<-EOT
+source:
+  type: Webhook
+  spec:
+    # TODO: support other SCM types
+    type: Github
+    spec:
+      type: Push
+      spec:
+        connectorRef: {{ $.Connectors.Repo }}
+        autoAbortPreviousExecutions: false
+        payloadConditions:
+          - key: targetBranch
+            operator: Equals
+            value: ${each.value.branch}
+        headerConditions: []
+        repoName: {{ $.Org.Name }}/${each.key}
+        actions: []
+inputYaml: |
+  pipeline:
+    identifier: ${module.pipelines[each.key].details.id}
+    properties:
+      ci:
+        codebase:
+          build:
+            type: branch
+            spec:
+              branch: <+trigger.branch>
+  EOT  
+}
+
+module "trigger_tag" {
+  for_each = local.projects
+  
+  source  = "harness-community/content/harness//modules/triggers"
+  version = "~> 0.1"
+
+  name = "Tag"
+  organization_id = module.organization.details.id
+  project_id      = module.projects[each.key].details.id
+  pipeline_id     = module.pipelines[each.key].details.id
+  yaml_data       = <<-EOT
+source:
+  type: Webhook
+  spec:
+    # TODO: support other SCM types
+    type: Github
+    spec:
+      type: Push
+      spec:
+        connectorRef: {{ $.Connectors.Repo }}
+        autoAbortPreviousExecutions: false
+        payloadConditions:
+          - key: <+trigger.payload.ref>
+            operator: StartsWith
+            value: refs/tags/
+        headerConditions: []
+        repoName: {{ $.Org.Name }}/${each.key}
+        actions: []
+inputYaml: |
+  pipeline:
+    identifier: ${module.pipelines[each.key].details.id}
+    properties:
+      ci:
+        codebase:
+          build:
+            type: branch
+            spec:
+              branch: <+trigger.branch>
+  EOT
+}
+
 {{ range .Org.Projects -}}
 {{ if .Secrets -}}
 module "project_{{ slugify .Name }}_secrets" {

cbae328afb693cfb8ad1f6ed9f3dd264b554b8e7
Jim Sheldon <james.sheldon@harness.io>
2023-05-23 03:42:19 -0400

    test: terraform kubernetes pipeline test (#97)
    split docker and kubernetes terraform tests, add kubernetes test

--- cmd/terraform/terraform_test.go
+++ cmd/terraform/terraform_test.go
@@ -9,54 +9,84 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	tests, err := filepath.Glob("testdata/examples/*.json")
-	if err != nil {
-		t.Error(err)
-		return
-	}
 
-	for _, test := range tests {
-		testDir, testFile := filepath.Split(test)
-		t.Run(test, func(t *testing.T) {
-			c := terraformCommand{
-				input:           test,
-				account:         "DNVIhrzCr9SnPHMQUEvRspB",
-				endpoint:        "https://app.harness.io/gateway",
-				providerSource:  "harness/harness",
-				providerVersion: "0.19.1",
-				output:          testDir + "output/" + testFile + ".out",
-				organization:    "exampleOrg",
-				dockerConn:      "exampleDockerConn",
-				downgrade:       true,
-				orgSecrets:      true,
-			}
+	pipelineTypes := []string{"docker", "kubernetes"}
 
-			// Run the terraform config generation
-			err = c.run(nil)
-			if err != nil {
-				t.Error(err)
-				return
-			}
+	for _, pipelineType := range pipelineTypes {
 
-			// Read the output file
-			got, err := os.ReadFile(c.output)
-			if err != nil {
-				t.Error(err)
-				return
-			}
+		tests, err := filepath.Glob("testdata/examples/" + pipelineType + "/*.json")
+		if err != nil {
+			t.Error(err)
+			return
+		}
 
-			// Read the golden file
-			want, err := os.ReadFile(test + ".golden")
-			if err != nil {
-				t.Error(err)
-				return
-			}
+		for _, test := range tests {
+			testDir, testFile := filepath.Split(test)
+			t.Run(test, func(t *testing.T) {
+				c := terraformCommand{}
 
-			// Compare the output to the golden file
-			if diff := cmp.Diff(got, want); diff != "" {
-				t.Errorf("Unexpected conversion result")
-				t.Log(diff)
-			}
-		})
+				dockerTerraformCommand := terraformCommand{
+					input:           test,
+					account:         "DNVIhrzCr9SnPHMQUEvRspB",
+					endpoint:        "https://app.harness.io/gateway",
+					providerSource:  "harness/harness",
+					providerVersion: "0.19.1",
+					output:          testDir + "output/" + testFile + ".tf",
+					organization:    "exampleOrg",
+					dockerConn:      "exampleDockerConn",
+					downgrade:       true,
+					orgSecrets:      true,
+				}
+
+				kubernetesTerraformCommand := terraformCommand{
+					input:           test,
+					account:         "DNVIhrzCr9SnPHMQUEvRspB",
+					endpoint:        "https://app.harness.io/gateway",
+					providerSource:  "harness/harness",
+					providerVersion: "0.19.1",
+					output:          testDir + "output/" + testFile + ".tf",
+					organization:    "exampleOrg",
+					dockerConn:      "exampleDockerConn",
+					kubeName:        "exampleNamespace",
+					kubeConn:        "exampleKubeConn",
+					downgrade:       true,
+					orgSecrets:      true,
+				}
+
+				if pipelineType == "docker" {
+					c = dockerTerraformCommand
+				}
+				if pipelineType == "kubernetes" {
+					c = kubernetesTerraformCommand
+				}
+
+				// Run the terraform config generation
+				err = c.run(nil)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				// Read the output file
+				got, err := os.ReadFile(c.output)
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				// Read the golden file
+				want, err := os.ReadFile(test + ".golden")
+				if err != nil {
+					t.Error(err)
+					return
+				}
+
+				// Compare the output to the golden file
+				if diff := cmp.Diff(got, want); diff != "" {
+					t.Errorf("Unexpected conversion result")
+					t.Log(diff)
+				}
+			})
+		}
 	}
 }
